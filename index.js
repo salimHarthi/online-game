e0d097bb42f2df82454e972d7a4bd28daee7c63f
salimHarthi <salim.harthi@phaze.ro>
2020-11-22 10:51:25 +0400

    now creashed message will be sent to both

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 const app = express();
 const cors = require("cors");
-const port = 3000
-const ip = "localhost"
+const port = 3000;
+const ip = "localhost";
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, "static")));
 
-  const server  = app.listen(port, ip, function (
-    error
-  ) {
-    if (error) {
-      console.log.error("Unable to listen for connections", error);
-      process.exit(10);
-    }
-    console.log(
-      "express is listening on http://" +
-        ip +
-        ":" +
-        port
-    );
+const server = app.listen(port, ip, function (error) {
+  if (error) {
+    console.log.error("Unable to listen for connections", error);
+    process.exit(10);
+  }
+  console.log("express is listening on http://" + ip + ":" + port);
+});
+const io = require("socket.io")(server);
+io.on("connection", (socket) => {
+  console.log("connect");
+
+  socket.on("move", (data) => {
+    socket.broadcast.emit("move", data);
   });
-  const io = require('socket.io')(server);
-  io.on("connection", (socket) => {
-    console.log("connect")
-
-    socket.on("move", (data) => {
-      socket.broadcast.emit("move",data)
-      //io.emit('move', data);
-    });
-
-})
-
-  app.use(function (req, res, next) {
-    req.io = io;
-    next();
+  socket.on("crash", (data) => {
+    io.emit("crash", data);
   });
+});
 
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/static/index.html');
-   });
-   
- 
+app.use(function (req, res, next) {
+  req.io = io;
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/static/index.html");
+});
--- static/js/io.js
+++ static/js/io.js
@@ -1,33 +1,70 @@
+$(document).ready(function () {
+  const socket = io("http://localhost:3000/");
 
-$( document ).ready(function() {
-  const socket = io('http://localhost:3000/');
+  class Otherplayer {
+    move() {
+      ctx.clearRect(move[0].old, move[1].old, PIXELSIZE, PIXELSIZE);
+      ctx.fillRect(move[0].new, move[1].new, PIXELSIZE, PIXELSIZE);
+    }
+  }
+  class Player {
+    constructor(name) {
+      //this.color = color;
+      this.name = name;
+    }
+    move(key) {
+      postionX.old = postionX.new;
+      postionY.old = postionY.new;
+      switch (key.charCode) {
+        case a:
+          postionX.new -= PIXELSIZE;
+          break;
+        case s:
+          postionY.new += PIXELSIZE;
+          break;
+        case w:
+          postionY.new -= PIXELSIZE;
+          break;
+        case d:
+          postionX.new += PIXELSIZE;
+          break;
+        default:
+      }
+      postionX.new = postionX.new < 0 ? 0 : postionX.new;
+      postionY.new = postionY.new < 0 ? 0 : postionY.new;
+      postionX.new = postionX.new > widthMax ? widthMax : postionX.new;
+      postionY.new = postionY.new > hightMax ? hightMax : postionY.new;
 
+      ctx.clearRect(postionX.old, postionY.old, PIXELSIZE, PIXELSIZE);
+      ctx.fillRect(postionX.new, postionY.new, PIXELSIZE, PIXELSIZE);
+      socket.emit("move", [postionX, postionY, this.name]);
+    }
+  }
 
-
-
-// setup 
+  // setup
+  let myplayer = new Player("salim");
   let canvas = $("#myCanvas");
   let ctx = canvas.get(0).getContext("2d");
   let canvasWidth = canvas.width();
   let canvasHeight = canvas.height();
-  let DIMENSION = 25
+  let DIMENSION = 25;
   let PIXELSIZE = canvasWidth / DIMENSION;
-  let selectedColor = '#222244';
+  let selectedColor = "#222244";
   let enabled = true;
   let filledPixels = {};
-  let postionX = {old:0, new:0}
-  let postionY = {old:0, new:0}
-  let widthMax= canvasWidth-PIXELSIZE
-  let hightMax= canvasHeight-PIXELSIZE
-  ctx.strokeStyle = 'rgba(0,0,0,0.1)';
+  let postionX = { old: 0, new: 0 };
+  let postionY = { old: 0, new: 0 };
+  let widthMax = canvasWidth - PIXELSIZE;
+  let hightMax = canvasHeight - PIXELSIZE;
+  ctx.strokeStyle = "rgba(0,0,0,0.1)";
   for (let i = 0; i < DIMENSION; ++i) {
-    x = Math.floor(i * canvasWidth / DIMENSION);
+    x = Math.floor((i * canvasWidth) / DIMENSION);
     ctx.beginPath();
     ctx.moveTo(x, 0);
     ctx.lineTo(x, canvasHeight);
     ctx.stroke();
 
-    y = Math.floor(i * canvasHeight / DIMENSION);
+    y = Math.floor((i * canvasHeight) / DIMENSION);
     ctx.beginPath();
     ctx.moveTo(0, y);
     ctx.lineTo(canvasWidth, y);
@@ -35,57 +72,30 @@ $( document ).ready(function() {
   }
   ctx.fillRect(postionX.old, postionY.old, PIXELSIZE, PIXELSIZE);
 
-// key setup 
-const a = 97
-const s= 115
-const w= 119
-const d = 100
-  const move= (key)=>{
-    postionX.old = postionX.new
-    postionY.old = postionY.new
-    switch(key.charCode) {
-      case a:
-        postionX.new-=PIXELSIZE
-        break;
-      case s:
-        postionY.new+=PIXELSIZE
-        break;
-      case w:
-        postionY.new-=PIXELSIZE
-          break;
-      case d:
-        postionX.new+=PIXELSIZE
-            break;
-      default:
-        
-    }
-    postionX.new = postionX.new<0?0:postionX.new
-    postionY.new = postionY.new<0?0:postionY.new
-    postionX.new = postionX.new>widthMax?widthMax:postionX.new
-    postionY.new = postionY.new>hightMax?hightMax:postionY.new
-    
-    ctx.clearRect(postionX.old, postionY.old, PIXELSIZE, PIXELSIZE);
-    ctx.fillRect(postionX.new, postionY.new, PIXELSIZE, PIXELSIZE);
-    socket.emit('move', [postionX,postionY])
-  }
-  $(document).on('keypress',function(e) {
-    move(e)
-    
+  // key setup
+  const a = 97;
+  const s = 115;
+  const w = 119;
+  const d = 100;
+
+  $(document).on("keypress", function (e) {
+    myplayer.move(e);
   });
 
   $(function () {
-    socket.on('move', function(move){
+    socket.on("move", function (move) {
       ctx.clearRect(move[0].old, move[1].old, PIXELSIZE, PIXELSIZE);
       ctx.fillRect(move[0].new, move[1].new, PIXELSIZE, PIXELSIZE);
-      crash(move)
+      crash(move);
     });
+    socket.on("crash", function () {
+      alert("crashed");
+    });
+  });
 
-  })
-
-const crash = (other) =>{
-  if ( other[0].new == postionX.new && other[1].new == postionY.new){
-    alert("crashed");
-  }
-} 
+  const crash = (other) => {
+    if (other[0].new == postionX.new && other[1].new == postionY.new) {
+      socket.emit("crash", [postionX, postionY, this.name]);
+    }
+  };
 });
-
