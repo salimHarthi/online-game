cf25b278f96efde909225c821ef7bbd15ed0fe70
salimHarthi <54818049+salimHarthi@users.noreply.github.com>
2020-11-22 20:54:42 +0400

    process.env.PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const cors = require("cors");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const ip = "localhost";
 app.use(cors());
 
